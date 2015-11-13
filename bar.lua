fa6fab60bb7dd577abb36e9fc080994c357f72f9
internaught <scott@internaught.io>
2015-11-13 09:22:03 -0600

    :notes:

--- bar.lua
+++ bar.lua
@@ -30,13 +30,16 @@ for _,screen in ipairs(hs.screen.allScreens()) do
 
         box:setFillColor({["red"]=0.1,["blue"]=0.1,["green"]=0.1,["alpha"]=1}):setFill(true):show()
 -- Create the iTunes box
-        local text = hs.drawing.text(hs.geometry.rect(frame.x + 2, frame.y + 2, frame.w -2, 20), "")
+        local text = hs.drawing.text(hs.geometry.rect(frame.x + 2, frame.y + 2, frame.w - 2, 20), "")
         table.insert(iTunesBoxes, text)
 -- Set the text color
         text:setTextColor({["red"]=1,["blue"]=1,["green"]=1,["alpha"]=1})
 -- Set the font size and font type
         text:setTextSize(14)
         text:setTextFont('Tamzen7x14')
+-- Tuck it in, under notifications, under the "real" menubar
+        box:setLevel(hs.drawing.windowLevels["floating"])
+        text:setLevel(hs.drawing.windowLevels["floating"])
 -- Show it off
         text:show()
 end
